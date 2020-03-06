b0ec3d7fe96bcbe856bdcf75c6a9142d01e4a7c8
Jbcalibur <valentijb@gmail.com>
2020-03-06 12:14:05 +0100

    init

---
+++ blocks/block.py
@@ -0,0 +1,74 @@
+import pygame
+from enum import Enum
+from color import Color
+
+
+class BlockShape(Enum):
+    L = (((0, 50), (100, 50), (100, 0), (150, 0), (150, 100), (0, 100)),
+         ((0, 75), (100, 25), (150, 25), (150, 75)),
+         ((25, 50), (75, 50), (125, 0), (125, 100), (25, 100), (75, 100)),
+         (151, 101))
+    J = (((0, 0), (150, 0), (150, 100), (100, 100), (100, 50), (0, 50)),
+         ((0, 0),),
+         ((0, 0),),
+         (151, 101))
+    O = (((0, 0), (100, 0), (100, 100), (0, 100)),
+         ((0, 0),),
+         ((0, 0),),
+         (101, 101))
+    I = (((0, 0), (50, 0), (50, 150), (0, 150)),
+         ((0, 0),),
+         ((0, 0),),
+         (51, 151))
+    Z = (((0, 0), (100, 0), (100,50), (150,50), (150,100), (50,100), (50,50), (0,50)),
+         ((0, 0),),
+         ((0, 0),),
+         (151, 101))
+    S = (((150, 0), (150, 50), (100,50), (100,100), (0,100), (0,50), (50,50), (50,0)),
+         ((0, 0),),
+         ((0, 0),),
+         (151, 101))
+    T = (((0, 0), (150, 0), (150, 50), (100, 50), (100, 100), (50, 100), (50, 50), (0, 50)),
+         ((0, 0),),
+         ((0, 0),),
+         (151, 101))
+
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, parent, color, shape, position):
+        pygame.sprite.Sprite.__init__(self)
+        self.parent = parent
+        self.image = pygame.Surface(
+            shape.value[-1], pygame.SRCALPHA, 32).convert_alpha()
+        self.rect = pygame.draw.polygon(self.image, color, shape.value[0])
+        for dot in shape.value[1]:
+            self.image.set_at(dot, Color.RED.value)
+        for dot in shape.value[2]:
+            self.image.set_at(dot, Color.BLUE.value)
+        self.rect.x = position[0]
+        self.rect.y = position[1]
+        self.move = 0
+        self.move_speed = 5
+
+    def update(self):
+        side_hit_list = pygame.sprite.spritecollide(
+            self, self.parent.block_list, False)[1:]
+        floor_hit_list = pygame.sprite.spritecollide(
+            self, self.parent.block_list, False)[1:]
+        if self.rect.bottom <= self.parent.size[1] and len(floor_hit_list) == 0:
+            # self.rect.y += self.move_speed
+            pass
+        if (self.rect.left + self.move >= 0) and (self.rect.right + self.move <= self.parent.size[0]+self.move_speed) and len(side_hit_list) == 0:
+            self.rect.x += self.move
+
+    def rotate(self, angle):
+        self.image = pygame.transform.rotate(self.image, angle)
+
+    def start_move(self, direction):
+        if direction == "left":
+            self.move = -self.move_speed
+        else:
+            self.move = self.move_speed
+
+    def stop_move(self):
+        self.move = 0
---
+++ main.py
@@ -0,0 +1,107 @@
+import pygame
+from pygame import locals as pygame_locals
+from blocks.block import Block, BlockShape
+from functools import partial
+from color import Color
+
+__inst__ = None
+
+
+def create_application(name, size):
+    global __inst__
+    if __inst__ is None:
+        __inst__ = Application(name, size)
+        return __inst__
+    else:
+        raise Exception("Application already exist")
+
+
+def get_application():
+    global __inst__
+    if __inst__ is not None:
+        return __inst__
+    else:
+        raise Exception("Application doesn't exist")
+
+
+class Application():
+    def __init__(self, name, size):
+        pygame.init()
+        self.name = name
+        self.size = size
+        self.in_progress = False
+        pygame.display.set_caption(self.name)
+        self.window = pygame.display.set_mode(self.size)
+        self.window.fill(Color.WHITE.value)
+        self.clock = pygame.time.Clock()
+        self.event_map = {
+            pygame_locals.QUIT: self._stop,
+            pygame_locals.KEYDOWN: self._handle_key_down,
+            pygame_locals.KEYUP: self._handle_key_up,
+        }
+        self.current = None
+        self.block_list = pygame.sprite.Group()
+        self.all_sprites_list = pygame.sprite.Group()
+
+    def start(self):
+        self.in_progress = True
+
+        self.current = Block(self, Color.SILVER.value, BlockShape.T, (50, 300))
+        self.block_list.add(self.current)
+        self.all_sprites_list.add(self.current)
+
+        self.current = Block(self, Color.YELLOW.value, BlockShape.L, (0, 0))
+        self.current.rotate(180)
+        self.block_list.add(self.current)
+        self.all_sprites_list.add(self.current)
+
+        pygame.display.update()
+        while self.in_progress:
+            for event in pygame.event.get():
+                if event.type in self.event_map:
+                    self.event_map[event.type](event)
+            self.window.fill(Color.WHITE.value)
+            self.all_sprites_list.update()
+            self.all_sprites_list.draw(self.window)
+            self.clock.tick(30)
+            pygame.display.flip()
+        pygame.quit()
+
+    def _stop(self, event):
+        self.in_progress = False
+
+    def _handle_key_down(self, event):
+        key_map = {
+            276: partial(self._move_curent, "left", True),
+            273: partial(self._rotate_current, "left"),
+            275: partial(self._move_curent, "right", True),
+            274: partial(self._rotate_current, "right"),
+        }
+        if event.key in key_map:
+            key_map[event.key]()
+        print("down {}".format(event.key))
+
+    def _handle_key_up(self, event):
+        key_map = {
+            276: partial(self._move_curent, "left", False),
+            275: partial(self._move_curent, "right", False),
+        }
+        if event.key in key_map:
+            key_map[event.key]()
+        print("up {}".format(event.key))
+    
+    def _move_curent(self, direction, state):
+        if state:
+            self.current.start_move(direction)
+        else:
+            self.current.stop_move()
+
+    def _rotate_current(self, direction):
+        if direction == "left":
+            self.current.rotate(-90)
+        else:
+            self.current.rotate(90)
+
+
+if __name__ == "__main__":
+    create_application("Tetris", (500, 500)).start()
